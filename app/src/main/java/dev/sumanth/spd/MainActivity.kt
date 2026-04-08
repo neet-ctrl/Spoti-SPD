0dbc76dbaa385fefae63aae3e62e73f639ba17ac
Shakti-ctrl <a71991932@gmail.com>
2026-04-08 12:32:39 +0000

    Add missing imports for fillMaxSize and clip

--- app/src/main/java/dev/sumanth/spd/MainActivity.kt
+++ app/src/main/java/dev/sumanth/spd/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
